fb860dd40d7cc322b2fb8b2a1355f896838f56e3
Luis <luisjosepr@gmail.com>
2024-11-14 12:17:48 -0300

    fix

--- frontend/src/pages/components/postCard.tsx
+++ frontend/src/pages/components/postCard.tsx
@@ -49,13 +49,13 @@ export const PostCard = ({
         >
           <path
             stroke-linejoin="round"
-            stroke-linecap="round"
+            strokeLinecap="round"
             stroke-width="1.5"
             d="M7.39999 6.32003L15.89 3.49003C19.7 2.22003 21.77 4.30003 20.51 8.11003L17.68 16.6C15.78 22.31 12.66 22.31 10.76 16.6L9.91999 14.08L7.39999 13.24C1.68999 11.34 1.68999 8.23003 7.39999 6.32003Z"
           ></path>
           <path
             stroke-linejoin="round"
-            stroke-linecap="round"
+            strokeLinecap="round"
             stroke-width="1.5"
             d="M10.11 13.6501L13.69 10.0601"
           ></path>
