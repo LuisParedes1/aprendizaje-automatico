c7d2a9e6a09c9243f40b7db8b13f0b35aeea5b53
Luis <luisjosepr@gmail.com>
2024-11-17 16:27:22 -0300

    wip

--- frontend/src/pages/models/models.tsx
+++ frontend/src/pages/models/models.tsx
@@ -1,5 +1,5 @@
 export const MODELS: { value: string; name: string }[] = [
   { value: "logistic_regression_model", name: "Logistic Regression" },
   { value: "multinomial_nb_model", name: "Multinomial NB" },
-  // { value: "extra_trees_model", name: "Extra Trees Classifier"
+  // { value: "extra_trees_model", name: "Extra Trees Classifier" },
 ];
